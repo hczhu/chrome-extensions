7b8dbd8d79566ce71ea44c5e78ca9316192e3cdc
Hongcheng Zhu <hcz@fb.com>
2017-10-21 22:36:12 -0700

    make it more robust.

--- process-selected-text/process_selection.js
+++ process-selected-text/process_selection.js
@@ -1,27 +1,29 @@
-function processDom(root, callback) {
-  console.log("processing root with " + root.children.length + " children.");
+function processDom(root, node_id, callback) {
+  // console.log("processing root with " + root.children.length + " children.");
   if (root.children.length == 0 && root.textContent != null && root.textContent != '') {
-    console.log("Processing root text: [" + root.textContent + "]");
-    root.textContent = callback(root.textContent);
+    // console.log("Processing root text: [" + root.textContent + "]");
+    callback(root, node_id++);
   }
   for (var i = 0; i < root.children.length; ++i) {
     child = root.children[i];
     if (child.nodeType == Node.TEXT_NODE) {
-      console.log("Processing child text: [" + child.textContent + "]");
-      child.textContent = callback(child.textContent);
+      // console.log("Processing child text: [" + child.textContent + "]");
+      callback(child, node_id++);
     } else {
-      processDom(child, callback);
+      node_id = processDom(child, node_id, callback);
     }
   }
+  return node_id;
 }
 
 function processSeletion(callback) {
   selection = window.getSelection();
   // console.log(selection.toString());
+  var node_id = Date.now();
   if (selection.rangeCount) {
     for (var i = 0; i < selection.rangeCount; ++i) {
       root = selection.getRangeAt(i).commonAncestorContainer
-      processDom(root, callback);
+      node_id = processDom(root, node_id, callback);
     }
   } else if (selection.type == "Text") {
     alert("Doesn't work for selected text only");
@@ -55,27 +57,37 @@ chrome.runtime.onMessage.addListener(
       chrome.storage.local.set({
           '__denom__': request.denominator,
       });
-      callback = function(text) {
-        var oldText = text;
-        number = parseNumber(text);
+      callback = function(textNode, node_id) {
+        if (textNode.getAttribute('node_id') != null) {
+          console.log("Skip an already processed node: " + textNode.textContent);
+          return;
+        }
+        var oldText = textNode.textContent;
+        number = parseNumber(oldText);
         if (number == null) {
-        return text;
+          return;
         }
         newText = (number / denominator).toFixed(2);
+        textNode.textContent = newText;
+        textNode.setAttribute('node_id', node_id);
         chrome.storage.local.set({
-            [newText]: oldText,
+            [node_id]: oldText,
         });
-        return newText;
       };
     } else if (request.cmd == 'restore') {
-      callback = function(newText) {
-        chrome.storage.local.get(newText, function(result) {
-          if (result.hasOwnProperty(newText)) {
-            console.log("Get [" + result[newText] + "] from the local storage for key: [" + newText + "].");
-            return result[newText];
+      callback = function(textNode, node_id) {
+        node_id = textNode.getAttribute('node_id');
+        if (node_id == null) {
+          return;
+        }
+        chrome.storage.local.get(node_id, function(result) {
+          if (result.hasOwnProperty(node_id)) {
+            // console.log("Get [" + result[newText] + "] from the local storage for key: [" + newText + "].");
+            textNode.textContent = result[node_id];
           }
-          return newText;
         });
+        textNode.removeAttribute('node_id');
+        chrome.storage.local.remove(node_id);
       };
     }
     processSeletion(callback);
