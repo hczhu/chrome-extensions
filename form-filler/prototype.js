ff3fed80c4ecdeb482ee108f146a8f077caee0f0
Hongcheng Zhu <hcz@fb.com>
2018-03-20 22:38:46 -0700

    update

---
+++ form-filler/prototype.js
@@ -0,0 +1,12 @@
+items = document.evaluate('//li[@ng-repeat="option in options"]', document, null, XPathResult.ANY_TYPE, null);
+nodes=[];
+itr = items.iterateNext();
+while (itr) {
+    ndoes.push(itr);
+    itr=items.iterateNext();
+}
+for (let i = 0; i<nodes.length; ++i) {
+    if (nodes[i].children[1].textContent == "No") {
+        nodes[i].children[0].checked = true;
+    }
+}
